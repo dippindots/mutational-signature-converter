638164ae75a69b6b802082a45a689742439a0c95
gaofei zhao <zgfilx@163.com>
2020-07-21 16:01:58 -0400

    update to SBS signatures

--- mutational_signature.sh
+++ mutational_signature.sh
@@ -18,7 +18,7 @@ conda activate mutational_signature
 ${PYTHON_PATH}python ${PROJECT_PATH}make_spectrum.py ${DATA_PATH}data_mutations_extended.txt ${PROJECT_PATH}msk_impact.tmp ${FASTA_FILE_PATH}hg19.fa
 
 # get output file
-Rscript ${PROJECT_PATH}signature.significance.R -i ${PROJECT_PATH}msk_impact.spectrum.txt --signature_file ${PROJECT_PATH}Stratton_signatures30.txt -o ${PROJECT_PATH}msk_impact.out -m ${PROJECT_PATH}signature.significance.stan -l ${R_LIBRARY}
+Rscript ${PROJECT_PATH}signature.significance.R -i ${PROJECT_PATH}msk_impact.spectrum.txt --signature_file ${PROJECT_PATH}sigProfiler_exome_SBS_signatures.txt -o ${PROJECT_PATH}msk_impact.out -m ${PROJECT_PATH}signature.significance.stan -l ${R_LIBRARY}
 
 # get tabular file
 Rscript ${PROJECT_PATH}summ_to_tab.R ${PROJECT_PATH}msk_impact.spectrum.txt ${PROJECT_PATH}msk_impact.out ${PROJECT_PATH}msk_impact_tab.out
