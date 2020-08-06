6f446d99876da7f0dfbdca5f6bca69feeded08f8
Gaofei Zhao <gzd23@mail.missouri.edu>
2020-08-06 14:23:17 -0400

    Update converter to V3

--- converter.py
+++ converter.py
@@ -24,7 +24,7 @@ df = df.transpose()
 df = df.rename({"Tumor_Sample_Barcode": "ENTITY_STABLE_ID"}, axis='index')
 
 # add name, description and statement columns
-mutatinoalSignatureDictionary = {
+mutatinoalSignatureDictionaryV2 = {
     "1": "Signature 1, the aging signature, is detected in this case.",
     "2": "Signature 2, the APOBEC signature, is detected in this case.  This signature often coccurs with signature 13, the other APOBEC signature",
     "3": "Signature 3, the signature of Homologous Recombination Repair deficiency is detected in this case.  This signature is most commonly associated with BRCA mutations",
@@ -57,6 +57,74 @@ mutatinoalSignatureDictionary = {
     "30": "Signature 30 is detected in this case. We are not confident that we are able to detect signature 30 in the IMPACT cohort."
 }
 
+mutatinoalSignatureDictionaryV3 = {
+    "SBS1": "Signature SBS1 is clock-like in that the number of mutations in most cancers and normal cells correlates with the age of the individual.",
+    "SBS2": "SBS2 is usually found in the same samples as SBS13. It has been proposed that activation of AID/APOBEC cytidine deaminases in cancer may be due to previous viral infection, retrotransposon jumping, or tissue inflammation.",
+    "SBS3": "SBS3 is strongly associated with germline and somatic BRCA1 and BRCA2 mutations and BRCA1 promoter methylation in breast, pancreatic, and ovarian cancers.",
+    "SBS4": "Although tobacco smoking causes multiple cancer types in addition to lung and head and neck, SBS4 has not been detected in many of these. SBS29 is found in cancers associated with tobacco chewing and appears different from SBS4.",
+    "SBS5": "SBS5 is clock-like in that the number of mutations in most cancers and normal cells correlates with the age of the individual.",
+    "SBS6": "SBS6 is one of seven mutational signatures associated with defective DNA mismatch repair (with microsatellite instability, MSI) and is often found in the same samples as other MSI associated signatures: SBS14, SBS15, SBS20, SBS21, SBS26, and SBS44.",
+    "SBS7a": "SBS7a/SBS7b/SBS7c/SBS7d are found in cancers of the skin from sun exposed areas and are thus likely to be due to exposure to ultraviolet light. SBS7a may possibly be the consequence of just one of the two major known UV photoproducts, cyclobutane pyrimidine dimers or 6-4 photoproducts.",
+    "SBS7b": "SBS7a/SBS7b/SBS7c/SBS7d are found in cancers of the skin from sun exposed areas and are likely to be due to exposure to ultraviolet light. SBS7b may possibly be the consequence of just one of the two major known UV photoproducts, cyclobutane pyrimidine dimers or 6-4 photoproducts.",
+    "SBS7c": "SBS7a/SBS7b/SBS7c/SBS7d are found in cancers of the skin from sun exposed areas and are likely to be due to exposure to ultraviolet light.",
+    "SBS7d": "SBS7a/SBS7b/SBS7c/SBS7d are found in cancers of the skin from sun exposed areas and are likely to be due to exposure to ultraviolet light.",
+    "SBS8": "Unknown.",
+    "SBS9": "Chronic lymphocytic leukaemias that possess immunoglobulin gene hypermutation (IGHV-mutated) have elevated numbers of mutations attributed to SBS9 compared to those that do not have immunoglobulin gene hypermutation.",
+    "SBS10a": "SBS10a/SBS10b usually generate large numbers of somatic mutations (>100 mutations per MB) and samples with these signatures have been termed hypermutators.",
+    "SBS10b": "Signature SBS10a/SBS10b usually generate large numbers of somatic mutations (>100 mutations per MB) and samples with these signatures have been termed hypermutators.",
+    "SBS11": "SBS11 usually generates large numbers of somatic mutations (>10 mutations per MB).",
+    "SBS12": "SBS12 usually contributes a small percentage (<20%) of the mutations observed in liver cancer samples.",
+    "SBS13": "SBS13 is usually found in the same samples as SBS2. It has been proposed that activation of AID/APOBEC cytidine deaminases in cancer may be due to previous viral infection, retrotransposon jumping, or tissue inflammation.",
+    "SBS14": "SBS14 mutations are present in very high numbers in all samples in which it has been observed. SBS14 is one of seven mutational signatures associated with defective DNA mismatch repair (MSI) and is often found in the same samples as other MSI associated signatures: SBS6, SBS15, SBS20, SBS21, SBS26 and SBS44.",
+    "SBS15": "SBS15 is one of seven mutational signatures associated with defective DNA mismatch repair (MSI) and is often found in the same samples as other MSI associated signatures: SBS6, SBS14, SBS20, SBS21, SBS26, and SBS44.",
+    "SBS16": "In addition to lower levels of nucleotide excision repair on the untranscribed strands of genes, elevated levels of DNA damage on untranscribed strands (compared to the remainder of the genome) may contribute to SBS16. Contamination by SBS16 may still be present in the current version of SBS5.",
+    "SBS17a": "Unknown.",
+    "SBS17b": "SBS17b has similarities to SBS28 and these two signatures can be mistaken for one another.",
+    "SBS18": "Similar in profile to SBS36 which is associated with defective base excision repair due to MUTYH mutations.",
+    "SBS19": "Unknown.",
+    "SBS20": "SBS20 is one of seven mutational signatures associated with defective DNA mismatch repair (MSI) and is often found in the same samples as other MSI associated signatures: SBS6, SBS14, SBS15, SBS21, SBS26, and SBS44.",
+    "SBS21": "SBS21 is one of seven mutational signatures associated with defective DNA mismatch repair (MSI) and is often found in the same samples as other MSI associated signatures: SBS6, SBS14, SBS15, SBS20, SBS26, and SBS44.",
+    "SBS22": "Aristolochic acid exposure. Found in cancer samples with known exposures to aristolochic acid and the pattern of mutations exhibited by the signature is consistent with that observed in experimental systems of aristolochic acid exposure.",
+    "SBS23": "Unknown.",
+    "SBS24": "Aflatoxin exposure. SBS24 has been found in cancer samples with known exposures to aflatoxin and the pattern of mutations exhibited by the signature is consistent with that observed in experimental systems exposed to aflatoxin.",
+    "SBS25": "This signature has only been identified in Hodgkin's cell lines. Data is not available from primary Hodgkin lymphomas.",
+    "SBS26": "SBS26 is one of seven mutational signatures associated with defective DNA mismatch repair (MSI) and is often found in the same samples as other MSI associated signatures: SBS6, SBS14, SBS15, SBS20, SBS21, and SBS44.",
+    "SBS27": "Possible sequencing artefact.",
+    "SBS28": "SBS28 has similarities to SBS17b and these two signatures can be mistaken for one another. Signature SBS28 is found in most samples with SBS10a/SBS10b where it contributes very high numbers of mutations. In contrast, SBS28 contributes much smaller number of mutations in samples lacking SBS10a/SBS10b.",
+    "SBS29": "The pattern of C>A mutations in SBS29 appears different from the pattern of mutations due to tobacco smoking reflected by SBS4.",
+    "SBS30": "SBS30 is due to deficiency in base excision repair due to inactivating mutations in NTHL1.",
+    "SBS31": "SBS31 exhibits a pattern of mutations similar to components of SBS35 and both may be due to platinum drug treatment.",
+    "SBS32": "Prior treatment with azathioprine to induce immunosuppression.",
+    "SBS33": "Unknown.",
+    "SBS34": "Unknown.",
+    "SBS35": "SBS35 exhibits a pattern of mutations that encompasses SBS31 and both may be due to platinum drug treatment.",
+    "SBS36": "Similar to SBS18, which has been proposed to be due to reactive oxygen species induced DNA damage.",
+    "SBS37": "Unknown.",
+    "SBS38": "Unknown. Found only in ultraviolet light associated melanomas suggesting potential indirect damage from UV-light.",
+    "SBS39": "Unknown.",
+    "SBS40": "Numbers of mutations attributed to SBS40 are correlated with patients' ages for some types of human cancer.",
+    "SBS41": "Unknown",
+    "SBS42": "Occupational exposure to haloalkanes.",
+    "SBS43": "Unknown. Possible sequencing artefact.",
+    "SBS44": "SBS44 is one of seven mutational signatures associated with defective DNA mismatch repair (MSI) and is often found in the same samples as other MSI associated signatures: SBS6, SBS14, SBS15, SBS20, SBS21, and SBS26.",
+    "SBS45": "Possible artefact due to 8-oxo-guanine introduced during sequencing.",
+    "SBS46": "Signature SBS46 was found commonly in colorectal cancers from early releases of TCGA (data released prior 2013).",
+    "SBS47": "SBS47 was found in cancer samples that were subsequently blacklisted for poor quality of sequencing data.",
+    "SBS48": "SBS48 was found in cancer samples that were subsequently blacklisted for poor quality of sequencing data.",
+    "SBS49": "Possible sequencing artefact.",
+    "SBS50": "SBS50 was found in cancer samples that were subsequently blacklisted for poor quality of sequencing data.",
+    "SBS51": "Possible sequencing artefact.",
+    "SBS52": "Possible sequencing artefact.",
+    "SBS53": "Signature SBS53 was found in cancer samples that were subsequently blacklisted for poor quality of sequencing data.",
+    "SBS54": "Possible sequencing artefact. Possible contamination with germline variants.",
+    "SBS55": "Possible sequencing artefact.",
+    "SBS56": "Possible sequencing artefact.",
+    "SBS57": "Possible sequencing artefact.",
+    "SBS58": "Potential sequencing artefact.",
+    "SBS59": "Possible sequencing artefact.",
+    "SBS60": "Possible sequencing artefact.",
+}
+
 displayNameDictionary = {
     "mean": "contribution",
     "confidence": "confidence"
@@ -67,7 +135,7 @@ def rowFuncForNameColumn(row):
         return "NAME"
     if row.name == "Nmut":
         return "Number of mutations"
-    return "signature " + row.name.split('_')[1]
+    return row.name.split('_')[1]
 
 def rowFuncForDescriptionColumn(row):
     if row.name == "ENTITY_STABLE_ID":
@@ -81,14 +149,14 @@ def rowFuncForUrlColumn(row):
         return "URL"
     if row.name == "Nmut":
         return "NA"
-    return "https://cancer.sanger.ac.uk/cosmic/signatures_v2"
+    return "https://cancer.sanger.ac.uk/cosmic/signatures/SBS/" + row.name.split('_')[1] + ".tt"
 
 def rowFuncForConfidenceStatementColumn(row):
     if row.name == "ENTITY_STABLE_ID":
         return "CONFIDENCE_STATEMENT"
     if row.name == "Nmut":
         return "NA"
-    return mutatinoalSignatureDictionary[row.name.split('_')[1]]
+    return mutatinoalSignatureDictionaryV3[row.name.split('_')[1]]
 
 df['NAME'] = df.apply(rowFuncForNameColumn, axis = 1)
 df['DESCRIPTION'] = df.apply(rowFuncForDescriptionColumn, axis = 1)
